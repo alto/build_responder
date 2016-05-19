28c545fed9133b2473627383b4920363f052ce23
Thorsten Böttger <boettger@mt7.de>
2016-05-20 11:50:52 +1200

    allow post requests

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,4 +1,5 @@
 get '/builds', to: 'builds#check'
+post '/builds', to: 'builds#check'
 get '/', to: 'home#index'
 # Configure your routes here
 # See: http://www.rubydoc.info/gems/hanami-router/#Usage
